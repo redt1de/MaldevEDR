361a3b01bf1a9aeb8801f8a2607f14410701df5c
redt1de <redtidesec@gmail.com>
2023-07-09 17:59:20 -0400

    save me

--- _external/DetoursNT-HookDll/EdrHooksDLL/hooks.h
+++ _external/DetoursNT-HookDll/EdrHooksDLL/hooks.h
@@ -28,11 +28,9 @@ EXTERN_C NTSTATUS NTAPI  HookedNtAllocateVirtualMemory(HANDLE ProcessHandle, PVO
             
             break;
         }
-       CHAR jout[1000];
-       /* static CHAR jout[1000];*/
-        sprintf(jout, "{\"Function\":\"NtAllocateVirtualMemory\",\"mode\":\"userland\",\"ReturnAddress\":\"0x%p\",\"Args\":{\"ProcessHandle\":\"0x%x\",\"BaseAddress\":\"0x%x\",\"ZeroBits\":\"0x%x\",\"RegionSize\":\"0x%x\",\"AllocationType\":\"0x%x\",\"Protect\":\"0x%02X\"}}", _ReturnAddress(), ProcessHandle, *BaseAddress, ZeroBits, *RegionSize, AllocationType, Protect);
-        //sprintf(jout, "{\"Function\":\"NtAllocateVirtualMemory\",\"mode\":\"userland\",\"Args\":{\"ProcessHandle\":\"0x%x\"}}", ProcessHandle);
-        //sprintf(jout, "{\"Function\":\"NtAllocateVirtualMemory\",\"mode\":\"userland\",\"Args\":{\"ProcessHandle\":\"ABSOLUTE CUNT\"}}");
+       CHAR jout[1000]; // overflow me please, fix later
+        sprintf(jout, " { \"Type\":0, \"Event\":{\"Function\":\"NtAllocateVirtualMemory\",\"mode\":\"userland\",\"ReturnAddress\":\"0x%p\",\"Args\":{\"ProcessHandle\":\"0x%x\",\"BaseAddress\":\"0x%x\",\"ZeroBits\":\"0x%x\",\"RegionSize\":\"0x%x\",\"AllocationType\":\"0x%x\",\"Protect\":\"0x%02X\"}}}", _ReturnAddress(), ProcessHandle, *BaseAddress, ZeroBits, *RegionSize, AllocationType, Protect);
+
         OutputDebugStringA(jout);
         //WritePipe(jout);
        //PipeTest();
@@ -42,37 +40,5 @@ EXTERN_C NTSTATUS NTAPI  HookedNtAllocateVirtualMemory(HANDLE ProcessHandle, PVO
        //return  OrigNtAllocateVirtualMemory(ProcessHandle, BaseAddress, ZeroBits, RegionSize, AllocationType, Protect);
     DecrementCurrentNestingLevel();
     return retval;
-
-}
-////////////////////////////////////////////////////////////// NtProtectVirtualMemory //////////////////////////////////////////////
-EXTERN_C NTSYSAPI NTSTATUS NTAPI NtProtectVirtualMemory(HANDLE ProcessHandle, PVOID* BaseAddress, ULONG NumberOfBytesToProtect, ULONG NewAccessProtection, PULONG OldAccessProtection);
-typedef NTSTATUS(NTAPI* fnNtProtectVirtualMemory)(HANDLE ProcessHandle, PVOID* BaseAddress, ULONG NumberOfBytesToProtect, ULONG NewAccessProtection, PULONG OldAccessProtection);
-
-static fnNtProtectVirtualMemory OrigNtProtectVirtualMemory;
-
-EXTERN_C NTSTATUS NTAPI  HookedNtProtectVirtualMemory(HANDLE ProcessHandle, PVOID* BaseAddress, ULONG NumberOfBytesToProtect, ULONG NewAccessProtection, PULONG OldAccessProtection)
-{
-    NTSTATUS retVal = OrigNtProtectVirtualMemory(ProcessHandle, BaseAddress, NumberOfBytesToProtect, NewAccessProtection, OldAccessProtection);
-
-    IncrementCurrentNestingLevel();
-
-    do {
-        if (GetCurrentNestingLevel() > 1) {
-            break;
-        }
-
-      /*  
-        - try char[2000]
-        - rtlcontext thunk
-        - 
-        */
-
-       // CHAR jout[1000];
-       // sprintf(jout, "{\"Function\":\"NtProtectVirtualMemory\",\"mode\":\"userland\",\"Args\":{\"ProcessHandle\":\"0x%x\",\"BaseAddress\":\"0x%x\",\"NumberOfBytesToProtect\":\"0x%x\",\"NewAccessProtection\":\"0x%x\",\"OldAccessProtection\":\"0x%x\"}}", ProcessHandle, BaseAddress, NumberOfBytesToProtect, NewAccessProtection, OldAccessProtection);
-       // WritePipeSingle(jout);
-    } while (false);
-
-    DecrementCurrentNestingLevel();
-    return  retVal;
 
 }
--- _external/DetoursNT-HookDll/EdrHooksDLL/main.cpp
+++ _external/DetoursNT-HookDll/EdrHooksDLL/main.cpp
@@ -11,6 +11,50 @@
 #pragma comment(lib,"ntdllp.lib") // using ntdllp.lib from WDK since it exports sprintf()
 #pragma comment(lib,"kernel32.lib")
 
+
+static ULONG_PTR g_ImageBase;
+static DWORD g_ImageSize;
+static ULONG_PTR g_NtdllBase;
+static DWORD g_NtdllSize;
+static ULONG_PTR g_kernel32Base;
+static DWORD g_kernel32Size;
+
+VOID GetBaseAddresses() {
+    PIMAGE_DOS_HEADER piDH;
+    PIMAGE_NT_HEADERS piNH;
+
+    g_NtdllBase = (ULONG_PTR)GetModuleHandle(TEXT("ntdll.dll"));
+    piDH = (PIMAGE_DOS_HEADER)g_NtdllBase;
+    piNH = (PIMAGE_NT_HEADERS)(g_NtdllBase + piDH->e_lfanew);
+
+    g_NtdllSize = piNH->OptionalHeader.SizeOfImage;
+
+    g_kernel32Base = (ULONG_PTR)GetModuleHandle(TEXT("kernel32.dll"));
+    if (g_kernel32Base) {
+        piDH = (PIMAGE_DOS_HEADER)g_kernel32Base;
+        piNH = (PIMAGE_NT_HEADERS)(g_kernel32Base + piDH->e_lfanew);
+        g_kernel32Size = piNH->OptionalHeader.SizeOfImage;
+    }
+}
+
+VOID SendModInfo(const char * name) {
+    ULONG_PTR dllBase;
+    DWORD dllSize;
+    PIMAGE_DOS_HEADER piDH;
+    PIMAGE_NT_HEADERS piNH;
+
+    dllBase = (ULONG_PTR)GetModuleHandleA(name);
+    if (dllBase != NULL) {
+        piDH = (PIMAGE_DOS_HEADER)dllBase;
+        piNH = (PIMAGE_NT_HEADERS)(dllBase + piDH->e_lfanew);
+        dllSize = piNH->OptionalHeader.SizeOfImage;
+        char addrs[100];
+        // {\"Type\":2, \"Module\":{\"Name\":\"%s\",\"Base\":\"0x%p\",\"Size\":\"0x%x\"}}}
+        sprintf(addrs, "{\"Type\":2,\"Module\":{\"Name\":\"%s\",\"Base\":\"0x%p\",\"Size\":\"0x%x\"}}", name, dllBase, dllSize);
+        OutputDebugStringA(addrs);
+    }
+}
+
 __declspec(dllexport) void __cdecl ExportedFunction(void)
 {
 }
@@ -25,9 +69,10 @@ EXTERN_C BOOL WINAPI NtDllMain(_In_ HINSTANCE hModule, _In_ DWORD dwReason, _In_
   switch (dwReason)
   {
     case DLL_PROCESS_ATTACH:
-        PipeInit();
-     
-      
+        //PipeInit();
+        SendModInfo("ntdll.dll");
+        SendModInfo("kernel32.dll");
+
       DetourTransactionBegin();
       DetourUpdateThread(NtCurrentThread());
 
@@ -37,7 +82,7 @@ EXTERN_C BOOL WINAPI NtDllMain(_In_ HINSTANCE hModule, _In_ DWORD dwReason, _In_
      // OrigNtWriteVirtualMemory = NtWriteVirtualMemory;
       //DetourAttach((PVOID*)&OrigNtWriteVirtualMemory, HookedNtWriteVirtualMemory);
 
-      OrigNtProtectVirtualMemory = NtProtectVirtualMemory;
+     // OrigNtProtectVirtualMemory = NtProtectVirtualMemory;
       //DetourAttach((PVOID*)&OrigNtProtectVirtualMemory, HookedNtProtectVirtualMemory); // NtProtectVirtualMemory is causing issues, app hangs
 
       DetourTransactionCommit();
@@ -49,7 +94,7 @@ EXTERN_C BOOL WINAPI NtDllMain(_In_ HINSTANCE hModule, _In_ DWORD dwReason, _In_
 
       DetourDetach((PVOID*)&OrigNtAllocateVirtualMemory, HookedNtAllocateVirtualMemory);
       //DetourDetach((PVOID*)&OrigNtWriteVirtualMemory, HookedNtWriteVirtualMemory);
-      DetourDetach((PVOID*)&OrigNtProtectVirtualMemory, HookedNtProtectVirtualMemory);
+     // DetourDetach((PVOID*)&OrigNtProtectVirtualMemory, HookedNtProtectVirtualMemory);
 
 
       DetourTransactionCommit();
